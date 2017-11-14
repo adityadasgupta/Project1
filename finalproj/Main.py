44e7a0fffbf059417beb551398a4034693c54182
Fmutnansky <32681597+Fmutnansky@users.noreply.github.com>
2017-11-14 11:53:23 -0600

    Update Main.py

--- finalproj/Main.py
+++ finalproj/Main.py
@@ -28,7 +28,97 @@ def weather():
         y >= 60.0
         colour = (255,150 - int(y),150 - int(y))
 
+from sense_hat import SenseHat
+import threading
+
+sense = SenseHat()
+
+black = (0,0,0)
+
+def weather():
+    threading.Timer(1800.0, weather).start()
+
+    x = sense.temp
+    z = sense.get_humidity
+    a = sense.get_pressure()
+
+    y = (x * 1.8) + 32
+
+    if y<0:
+        y = 0
+    elif y > 255:
+        y = 255
+
+    if y < 60.0:
+        colour = (int(y),int(y),255)
+    else:
+        y >= 60.0
+        colour = (255,150 - int(y),150 - int(y))
+
+from sense_hat import SenseHat
+import threading
+
+sense = SenseHat()
+
+black = (0,0,0)
+
+def weather():
+    threading.Timer(1800.0, weather).start()
+
+    x = sense.temp
+    z = sense.get_humidity
+    a = sense.get_pressure()
+
+    y = (x * 1.8) + 32
+
+    if y<0:
+        y = 0
+    elif y > 255:
+        y = 255
+
+    if y < 60.0:
+        colour = (int(y),int(y),255)
+    else:
+        y >= 60.0
+        colour = (255,150 - int(y),150 - int(y))
+
+    print("Temperature: " + "%.2f" % y)
+    print("Humidity: " + "%.2f" % z)
+    print("Pressure: " + "%.2f" % a)
+    string_value1 = str("%.2f" % y) + 'F'
+    sense.show_message(string_value1, text_colour=colour, back_colour=black, scroll_speed=0.05)
+
+    if y < 0.0:
+        sense.show_message('Stay Inside!', text_colour=(0,0,255), back_colour=black, scroll_speed=0.05)
+    elif y < 40.0:    
+        sense.show_message('Winter Clothes Recommended!', text_colour=colour, back_colour=black, scroll_speed=0.05)
+    elif y < 60.0:
+        sense.show_message('Jacket is Recomended!', text_colour=colour, back_colour=black, scroll_speed=0.05)
+    elif y < 100.0:
+        sense.show_message('Enjoy the Warmth!', text_colour=colour, back_colour=black, scroll_speed=0.05)
+    else:
+        y > 100.0
+        sense.show_message('Stay Inside!', text_colour=colour, back_colour=black, scroll_speed=0.05)
+
+weather()
     print("%.2f" % y)
+    string_value1 = str("%.2f" % y) + 'F'
+    sense.show_message(string_value1, text_colour=colour, back_colour=black, scroll_speed=0.05)
+
+    if y < 0.0:
+        sense.show_message('Stay Inside!', text_colour=(0,0,255), back_colour=black, scroll_speed=0.05)
+    elif y < 40.0:    
+        sense.show_message('Winter Clothes Recommended!', text_colour=colour, back_colour=black, scroll_speed=0.05)
+    elif y < 60.0:
+        sense.show_message('Jacket is Recomended!', text_colour=colour, back_colour=black, scroll_speed=0.05)
+    elif y < 100.0:
+        sense.show_message('Enjoy the Warmth!', text_colour=colour, back_colour=black, scroll_speed=0.05)
+    else:
+        y > 100.0
+        sense.show_message('Stay Inside!', text_colour=colour, back_colour=black, scroll_speed=0.05)
+
+weather()
+
     string_value1 = str("%.2f" % y) + 'F'
     sense.show_message(string_value1, text_colour=colour, back_colour=black, scroll_speed=0.05)
 
@@ -44,7 +134,7 @@ def weather():
         y > 100.0
         sense.show_message('Stay Inside!', text_colour=colour, back_colour=black, scroll_speed=0.05)
 
-weather()
+#weather()
 def update() :
     sense = sensehat()
     temp = sense.get_temperature()
